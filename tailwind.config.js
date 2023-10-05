da0121c94f57b2f1e0533c9010c54fdc74f82e1e
Fabio <thebloodshadow14@gmail.com>
2023-10-05 15:30:19 +0200

    ADD - html & css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        custom: ["'Open Sans'", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
